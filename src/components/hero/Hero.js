f80f5c92a0ad5543019ae6c3bf052a9603c0cca5
Alex Paul <al.9thousand@gmail.com>
2020-01-20 19:42:58 -0500

    pic shows up

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,9 +1,54 @@
+// import React from "react"
+// import { Jumbotron } from "react-bootstrap"
+// import me from "../../images/me.png"
+
+// //import custom styles
+// import "./hero.css"
+// import SocialRow from "../SocialRow/SocialRow"
+
+
+// const Hero = () => (
+//   <Jumbotron style={jumbo_styles} className="jumbo" fluid>
+//     <div className="jumbo_content">
+//       <h1>Hi, I'm Alex, Software Engineer and Web Developer based in New York.</h1>
+//       <br />
+//       <h3>
+//         Right now I'm using Javascript, Node.js, React.js and Gatsby.js to
+//         design and hack noice websites together on the internet.
+//       </h3>
+//       <img 
+//         src = {me}
+//         alt = {"sup"}
+//       />
+//       <p>Have a project you'd like to discuss?</p>
+//       <p>
+//         Let's chat{" "}
+//         <a href="mailto:john.doe@gmail.com">
+//           <span style={{ color: "blueviolet" }}>john.doe@gmail.com</span>
+//         </a>
+//       </p>
+//     </div>
+//     <div>
+//       <SocialRow />
+//     </div>
+//   </Jumbotron>
+// )
+
+// const jumbo_styles = {
+//   backgroundColor: "#fff",
+//   paddingLeft: "1rem",
+// }
+
+// export default Hero
+
 import React from "react"
 import { Jumbotron } from "react-bootstrap"
+import me from "../../images/me.png"
 
 //import custom styles
 import "./hero.css"
 import SocialRow from "../SocialRow/SocialRow"
+
 
 const Hero = () => (
   <Jumbotron style={jumbo_styles} className="jumbo" fluid>
@@ -14,6 +59,10 @@ const Hero = () => (
         Right now I'm using Javascript, Node.js, React.js and Gatsby.js to
         design and hack noice websites together on the internet.
       </h3>
+      <img 
+        src = {me}
+        alt = {"sup"}
+      />
       <p>Have a project you'd like to discuss?</p>
       <p>
         Let's chat{" "}
@@ -21,9 +70,6 @@ const Hero = () => (
           <span style={{ color: "blueviolet" }}>john.doe@gmail.com</span>
         </a>
       </p>
-    </div>
-    <div>
-      <SocialRow />
     </div>
   </Jumbotron>
 )
